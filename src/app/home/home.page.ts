efae302fa713e253bf12bca23e66753b2316fd4c
Benicio Pareschi de Moraes <beniciopareschi@gmail.com>
2026-02-25 21:25:46 -0300

    Ac 1 Completa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,44 @@ export class HomePage {
 
   constructor() {}
 
+  vC='';
+  vF='';
+  lado1='';
+  lado2='';
+  lado3='';
+  n1='';
+  n2='';
+  n3='';
+  n4='';
+  res = '';
+  converterC(){
+    this.res = ((parseFloat(this.vC)*9 + 160)/5).toFixed(1)
+  }
+    converterF(){
+    this.res = ((parseFloat(this.vF) - 32) *(5/9)).toFixed(1)
+  }
+
+    verificarLado(){
+    if(this.lado1 == this.lado2 && this.lado1 == this.lado3 ){
+      this.res = "Este Triangulo é Equilátero"
+    } else if (this.lado1 == this.lado2 || this.lado1 == this.lado3 || this.lado2 == this.lado3 ) {
+      this.res = "Este Triangulo é Isóceles"
+    } else {
+      this.res = "Este Triangulo é Escaleno"
+    }
+  }
+calcularM() {
+  const media =
+    (parseFloat(this.n1) + parseFloat(this.n2) + parseFloat(this.n3) + parseFloat(this.n4)) / 4
+
+  this.res = media.toFixed(1)
+
+  if (media < 3) {
+    this.res = "Retido"
+  } else if (media >= 3 && media < 6) {
+    this.res = "Recuperação"
+  } else {
+    this.res = "Aprovado"
+  }
+}
 }
